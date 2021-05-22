2455b5f7afe968636cd5b8b0bc34549810ad5e06
Kohei <ohayousagi.ac.kook0727@gmail.com>
2021-05-22 20:53:01 +0900

    master

--- main.tf
+++ main.tf
@@ -12,3 +12,17 @@ module "apigateway" {
   source   = "./modules/apigateway"
   app_name = var.app_name
 }
+
+resource "aws_api_gateway_integration" "lambda_integration" {
+  rest_api_id             = module.apigateway.api_gateway_id
+  resource_id             = module.apigateway.api_gateway_resource_id
+  http_method             = "ANY"
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = module.lambda.lambda_invoke_arn
+}
+
+resource "aws_api_gateway_deployment" "example" {
+  rest_api_id = module.apigateway.api_gateway_id
+  stage_name  = "test"
+}
