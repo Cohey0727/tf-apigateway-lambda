eab63587765a7ddc12f8be2af1a6e68dda94fd03
Kohei <ohayousagi.ac.kook0727@gmail.com>
2021-05-23 12:50:13 +0900

    master

--- main.tf
+++ main.tf
@@ -13,16 +13,26 @@ module "apigateway" {
   app_name = var.app_name
 }
 
-resource "aws_api_gateway_integration" "lambda_integration" {
-  rest_api_id             = module.apigateway.api_gateway_id
-  resource_id             = module.apigateway.api_gateway_resource_id
-  http_method             = "ANY"
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = module.lambda.lambda_invoke_arn
+
+resource "aws_api_gateway_deployment" "rest_api" {
+  stage_name        = "api"
+  rest_api_id       = module.apigateway.api_gateway_id
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
-resource "aws_api_gateway_deployment" "example" {
-  rest_api_id = module.apigateway.api_gateway_id
-  stage_name  = "test"
+resource "aws_lambda_permission" "rest_api_invoke" {
+  function_name = module.lambda.lambda_arn
+  action        = "lambda:InvokeFunction"
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${module.apigateway.api_gateway_execution_arn}/*"
+}
+
+output "EndpointURL" {
+  value = aws_api_gateway_deployment.rest_api.invoke_url
+}
+
+output "RestAPIId" {
+  value = module.apigateway.api_gateway_id
 }
