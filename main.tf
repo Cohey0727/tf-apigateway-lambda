40cfa484998384613e088ff091fffc317584e191
Kohei <ohayousagi.ac.kook0727@gmail.com>
2021-05-23 16:34:33 +0900

    master

--- main.tf
+++ main.tf
@@ -15,8 +15,8 @@ module "apigateway" {
 
 
 resource "aws_api_gateway_deployment" "rest_api" {
-  stage_name        = "api"
-  rest_api_id       = module.apigateway.api_gateway_id
+  stage_name  = "api"
+  rest_api_id = module.apigateway.api_gateway_id
   lifecycle {
     create_before_destroy = true
   }
@@ -29,10 +29,32 @@ resource "aws_lambda_permission" "rest_api_invoke" {
   source_arn    = "${module.apigateway.api_gateway_execution_arn}/*"
 }
 
-output "EndpointURL" {
-  value = aws_api_gateway_deployment.rest_api.invoke_url
+resource "aws_api_gateway_resource" "resource" {
+  rest_api_id = module.apigateway.api_gateway_id
+  parent_id   = module.apigateway.api_gateway_root_resource_id
+  path_part   = "hello"
 }
 
-output "RestAPIId" {
-  value = module.apigateway.api_gateway_id
+resource "aws_api_gateway_method" "method" {
+  rest_api_id   = module.apigateway.api_gateway_id
+  resource_id   = aws_api_gateway_resource.resource.id
+  http_method   = "POST"
+  authorization = "NONE"
 }
+
+resource "aws_api_gateway_integration" "restapi_integration" {
+  rest_api_id             = module.apigateway.api_gateway_id
+  resource_id             = module.apigateway.api_gateway_root_resource_id
+  uri                     = module.lambda.lambda_invoke_arn
+  http_method             = aws_api_gateway_method.method.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+}
+
+# output "EndpointURL" {
+#   value = aws_api_gateway_deployment.rest_api.invoke_url
+# }
+
+# output "RestAPIId" {
+#   value = module.apigateway.api_gateway_id
+# }
